8113191eebee5dc281eed2127cb559ea2c641cb8
Paul Sydney <xtrycatchx@gmail.com>
2018-05-08 16:10:42 +0800

    init

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const seconds = Math.floor((new Date() - date) / 1000);
-const interval = Math.floor(seconds / 31536000);
 
-exports.boleh = (date) => {
+const seconds = date => Math.floor((new Date() - date) / 1000)
+
+exports.boleh = date => {
+    let interval = Math.floor(seconds(date) / 31536000);
     if (interval > 1) {
         return interval + " years ago";
     }
-    interval = Math.floor(seconds / 2592000);
+    interval = Math.floor(seconds(date) / 2592000);
     if (interval > 1) {
         return interval + " months ago";
     }
-    interval = Math.floor(seconds / 86400);
+    interval = Math.floor(seconds(date) / 86400);
     if (interval > 1) {
         return interval + " days ago";
     }
-    interval = Math.floor(seconds / 3600);
+    interval = Math.floor(seconds(date) / 3600);
     if (interval > 1) {
         return interval + " hours ago";
     }
-    interval = Math.floor(seconds / 60);
+    interval = Math.floor(seconds(date) / 60);
     if (interval > 1) {
         return interval + " minutes ago";
     }
-    return Math.floor(seconds) + " seconds ago";
+    return Math.floor(seconds(date)) + " seconds ago";
 }
